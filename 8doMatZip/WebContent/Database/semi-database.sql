5dc548fb252b1d3331b1d4b7206802f78c4dc762
kosta <kosta@192.168.0.166>
2017-10-13 10:06:48 +0900

    1013 db update

--- 8doMatZip/WebContent/Database/semi-database.sql
+++ 8doMatZip/WebContent/Database/semi-database.sql
@@ -1,37 +1,16 @@
 create table location(
 	locno varchar2(100) primary key,
 	loc varchar2(100) not null,
-	sigungu varchar2(100) not null,
-)
+	sigungu varchar2(100) not null
+);
+drop table location;
 create sequence locno_seq;
+drop sequence locno_seq;
 
-insert into location (loc, sigungu) values (locno_seq.nextval,'제주특별자치도','서귀포시');
-insert into location (loc, sigungu) values (locno_seq.nextval,'제주특별자치도','제주시');
-insert into location (loc, sigungu) values (locno_seq.nextval,'서울특별시','강동구');
-insert into location (loc, sigungu) values (locno_seq.nextval,'서울특별시','강남구');
-insert into location (loc, sigungu) values ('서울특별시','강북구');
-insert into location (loc, sigungu) values ('서울특별시','강서구');
-insert into location (loc, sigungu) values ('서울특별시','관악구');
-insert into location (loc, sigungu) values ('서울특별시','광진구');
-insert into location (loc, sigungu) values ('서울특별시','구로구');
-insert into location (loc, sigungu) values ('서울특별시','금천구');
-insert into location (loc, sigungu) values ('서울특별시','노원구');
-insert into location (loc, sigungu) values ('서울특별시','도봉구');
-insert into location (loc, sigungu) values ('서울특별시','동대문구');
-insert into location (loc, sigungu) values ('서울특별시','동작구');
-insert into location (loc, sigungu) values ('서울특별시','마포구');
-insert into location (loc, sigungu) values ('서울특별시','서대문구');
-insert into location (loc, sigungu) values ('서울특별시','서초구');
-insert into location (loc, sigungu) values ('서울특별시','성동구');
-insert into location (loc, sigungu) values ('서울특별시','성북구');
-insert into location (loc, sigungu) values ('서울특별시','송파구');
-insert into location (loc, sigungu) values ('서울특별시','양천구');
-insert into location (loc, sigungu) values ('서울특별시','영등포구');
-insert into location (loc, sigungu) values ('서울특별시','용산구');
-insert into location (loc, sigungu) values ('서울특별시','은평구');
-insert into location (loc, sigungu) values ('서울특별시','종로구');
-insert into location (loc, sigungu) values ('서울특별시','중구');
-insert into location (loc, sigungu) values ('서울특별시','중랑구');
+insert into location (locno,loc, sigungu) values (locno_seq.nextval,'제주특별자치도','서귀포시');
+insert into location (locno,loc, sigungu) values (locno_seq.nextval,'제주특별자치도','제주시');
+insert into location (locno,loc, sigungu) values (locno_seq.nextval,'서울특별시','강동구');
+insert into location (locno,loc, sigungu) values (locno_seq.nextval,'서울특별시','강남구');
 
 create table member(
 	mid varchar2(100) primary key,
@@ -40,9 +19,14 @@ create table member(
 	mname varchar2(100) not null,
 	maddress varchar2(100) not null,
 	mtel varchar2(100) not null,
-	constraint fk_member_auth foreign key(ano) references authority(ano),
+	constraint fk_member_auth foreign key(ano) references authority(ano)
 )
+insert into member(mid,ano,mpassword,mname,maddress,mtel)values('master','2','master','master','master','master');
+select * from member;
+drop table member;
+drop table board;
 
+select * from member;
 create table post(
 	pno varchar2(100) primary key,
 	mid varchar2(100) not null,
@@ -54,17 +38,17 @@ create table post(
 	pstar number not null,
 	locno varchar2(100) not null,
 	constraint fk_post_member foreign key(mid) references member(mid),
-	constraint fk_post_location foreign key(locno) references location(locno),
+	constraint fk_post_location foreign key(locno) references location(locno)
 )
 
-create table comment(
+create table comments(
 	comno varchar2(100) primary key,
 	pno varchar2(100) not null,
 	mid varchar2(100) not null,
 	comcontent clob not null,
 	comdate date not null,
 	constraint fk_com_member foreign key(mid) references member(mid),
-	constraint fk_com_post foreign key(pno) references post(pno),
+	constraint fk_com_post foreign key(pno) references post(pno)
 )
 
 create table request(
@@ -72,15 +56,9 @@ create table request(
 	mid varchar2(100) not null,
 	reqcontent clob not null,
 	reqdate date not null,
-	constraint fk_req_member foreign key(mid) references member(mid),
+	constraint fk_req_member foreign key(mid) references member(mid)
 )
 
-create table myfavorite(
-	mid varchar2(100) not null,
-	pno varchar2(100) not null,
-	constraint fk_myfav_member foreign key(mid) references member(mid),
-	constraint pk_member_post primary key (mid, pno)
-)
 create table post_myfav(
 	mid varchar2(100) not null,
 	pno varchar2(100) not null,
@@ -91,9 +69,10 @@ create table post_myfav(
 
 create table authority(
 	ano varchar2(100) primary key,
-	atype varchar2(100) not null,
+	atype varchar2(100) not null
 )
-
+insert into authority(ano,atype) values('1','회원');
+insert into authority(ano,atype) values('2','관리자');
 create table imagepath(
 	pno varchar2(100) not null,
 	ipath varchar2(100) not null,
@@ -104,7 +83,6 @@ create table imagepath(
 create table QNA(
 	mid varchar2(100) not null,
 	answer varchar2(100) not null,
-	question varchar2(100) not null,
 	constraint fk_qna_member foreign key(mid) references member(mid),
 	constraint pk_qna_member primary key (mid, answer)
 )
