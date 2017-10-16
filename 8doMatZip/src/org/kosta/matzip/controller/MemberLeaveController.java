489399d4bafe13a6ce2257f6c9bc1fb9da9546d1
kosta <kosta@192.168.0.166>
2017-10-16 16:55:18 +0900

    10160455

--- 8doMatZip/src/org/kosta/matzip/controller/MemberLeaveController.java
+++ 8doMatZip/src/org/kosta/matzip/controller/MemberLeaveController.java
@@ -2,7 +2,6 @@ package org.kosta.matzip.controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import org.kosta.matzip.model.MemberDAO;
 
@@ -12,10 +11,6 @@ public class MemberLeaveController implements Controller {
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		String mid=request.getParameter("mid");
 		MemberDAO.getInstance().memberLeave(mid);
-		/*HttpSession session=request.getSession(false);
-		if(session!=null)
-			session.invalidate();*/
-		//request.setAttribute("url", "cityTest.jsp");
 		return "redirect:DispatcherServlet?command=logout";
 	}
 
